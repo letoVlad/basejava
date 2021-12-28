02acdb63b084cb9ff7bb0dd18f1aa2f90169802c
evtepo@gmail.com <Lemana144>
2021-12-28 09:30:58 +0300

    HM

---
+++ src/ArrayStorage.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+    int i = 0;
+
+    Resume check(int index) {
+        return storage[index];
+    }
+
+    void clear() {
+    }
+
+    void save(Resume r) {
+        storage[i] = r;
+        i++;
+    }
+
+    Resume get(String uuid) {
+        return storage[Integer.parseInt(uuid)];
+    }
+
+    void delete(String uuid) {
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        return Arrays.copyOf(storage, i);
+
+    }
+
+    int size() {
+        return 0;
+    }
+}
