a47ca66c27b71b127ded9f439b7ad57c796503e4
sonic <vl040@mail.ru>
2022-01-11 12:04:24 +0300

    HM

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -4,57 +4,69 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-/**
- * Array based storage for Resumes
- */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10];
     private int size = 0;
 
 
-    void check(String uuid) {
+    public int check(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].equals(uuid)){
-
+            if (storage[i].getUuid() == uuid) {
+                return i;
             }
         }
+        return -1;
     }
 
     public void update(Resume resume) {
+        check(resume.getUuid());
         for (int i = 0; i < size; i++) {
             if (resume.equals(storage[i].getUuid())) {
-                System.out.println("нашли");
             }
         }
     }
 
     public void clear() {
-        for (int i = 0; i < size; i++) {
-            storage[i] = null;
-        }
+        Arrays.fill(storage, null);
         size = 0;
     }
 
     public void save(Resume r) {
-        storage[size] = r;
-        size++;
+        if (check(r.getUuid()) != -1) {
+            System.out.println("Данное резюме есть");
+        } else if (size == storage.length) {
+            System.out.println("Переполнен");
+        } else {
+            storage[size] = r;
+            size++;
+        }
     }
 
+
     public Resume get(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid() == uuid) {
-                return storage[i];
+        int index = check(uuid);
+        if (index != -1) {
+            System.out.println("Данное рюземе есть");
+            for (int i = 0; i < size; i++) {
+                if (storage[i].getUuid() == uuid) {
+                    return storage[i];
+                }
             }
         }
         return null;
     }
 
     public void delete(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].getUuid())) {
-                storage[size - 1] = null;
-                size--;
-                break;
+        int index = check(uuid);
+        if (index != -1) {
+            System.out.println("Данное рюземе есть");
+            for (int i = 0; i < size; i++) {
+                if (uuid.equals(storage[i].getUuid())) {
+                    storage[i] = storage[size - 1];
+                    storage[size - 1] = null;
+                    size--;
+                    break;
+                }
             }
         }
     }
@@ -70,3 +82,4 @@ public class ArrayStorage {
         return size;
     }
 }
+
