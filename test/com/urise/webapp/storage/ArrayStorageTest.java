39b8c3d9f7111942f1bb3a4813043091ece16735
evtepo@gmail.com <lemana144>
2022-02-15 11:56:07 +0300

    hm

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ArrayStorageTest extends AbstractArrayStorageTest {
+
     public ArrayStorageTest() {
         super(new ArrayStorage());
     }
--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.*;
 
 public class SortedArrayStorageTest extends AbstractArrayStorageTest {
 
-
     public SortedArrayStorageTest() {
         super(new SortedArrayStorage());
     }
